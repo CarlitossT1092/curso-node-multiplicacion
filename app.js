333b60b4f5f97b768f0d0fa497e42a03f8d43fbc
Carlos Torres <carlos.th1011@gmail.com>
2021-08-23 15:56:27 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//const fs = require('fs');
+const { crearArchivo } = require('./helpers/multiplicar');
+const colors = require('colors');
+const argv = require('./config/yargs');
+
+console.clear();
+
+//console.log( process.argv );
+console.log( argv );
+
+
+crearArchivo( argv.b, argv.l, argv.h )
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+    .catch( err => console.log(err));
